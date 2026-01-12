00238c880d5caea9e258508750bd836da888af7d
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-12 20:41:39 +0530

    create the booking repository

--- src/main/java/com/example/Hotel_Management_System/repository/BookingRepository.java
+++ src/main/java/com/example/Hotel_Management_System/repository/BookingRepository.java
@@ -8,4 +8,8 @@ import java.util.List;
 public interface BookingRepository extends JpaRepository<Booking,Long> {
 
     List<Booking> findAllByRoomId(Long roomId);
+
+    List<Booking>findByBookingConfirmationCode(String confirmationCode);
+
+    List<Booking>findByUserId(Long userId);
 }
