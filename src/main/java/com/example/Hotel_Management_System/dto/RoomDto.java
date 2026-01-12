392b48196c482e4cf542fe63cb0d0b8155033d5d
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-12 12:58:53 +0530

    creata user repository

--- src/main/java/com/example/Hotel_Management_System/dto/RoomDto.java
+++ src/main/java/com/example/Hotel_Management_System/dto/RoomDto.java
@@ -13,7 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_NULL)  //“When converting this object to JSON, exclude any fields that are null.”
 public class RoomDto {
     private long id;
     private String roomType;
@@ -22,3 +22,46 @@ public class RoomDto {
     private String roomDescription;
     private List<BookingDto> bookings = new ArrayList<>();
 }
+
+/**
+
+ import com.fasterxml.jackson.annotation.JsonInclude;
+
+ @JsonInclude(JsonInclude.Include.NON_NULL)
+ public class User {
+ private String name;
+ private String email;
+ private Integer age;
+
+ // getters & setters
+
+ }
+ ------------------------------------------------------------
+
+ User user = new User();
+ user.setName("Alice");
+ user.setEmail(null);
+ user.setAge(25);
+
+ JSON output (response)
+ {
+ "name": "Alice",
+ "age": 25
+ }
+
+
+ The email field is not included because it is null.
+
+
+ Without @JsonInclude(NON_NULL)
+
+ The JSON would look like:
+
+ {
+ "name": "Alice",
+ "email": null,
+ "age": 25
+ }
+
+
+ * **/
