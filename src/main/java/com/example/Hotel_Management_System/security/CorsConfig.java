c50e0cb2632b3850068fef9658ed62446c7fa53c
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-19 20:34:48 +0530

    create the JWTAuthFilter

--- src/main/java/com/example/Hotel_Management_System/security/CorsConfig.java
+++ src/main/java/com/example/Hotel_Management_System/security/CorsConfig.java
@@ -8,14 +8,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+                                                // This method creates a WebMvcConfigurer bean
+                                                // Used to customize Spring MVC behavior (CORS here)
     @Bean
-    public WebMvcConfigurer webMvcConfigurer(){
-        return  new WebMvcConfigurer() {
+    public WebMvcConfigurer webMvcConfigurer() {
+
+                                                // Anonymous class implementing WebMvcConfigurer
+        return new WebMvcConfigurer() {
+
+            // Configure CORS rules
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+
+                registry.addMapping("/**")// Allow CORS for all endpoints
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow these HTTP methods
+                        .allowedHeaders("*");// Allow all request headers
             }
         };
     }
