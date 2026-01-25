bbd4652fd9d67a2a9424183d1627956c985f6f5c
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-25 16:09:02 +0530

    create the user service get user information function

--- src/main/java/com/example/Hotel_Management_System/service/impl/UserService.java
+++ src/main/java/com/example/Hotel_Management_System/service/impl/UserService.java
@@ -173,7 +173,23 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public Response getMyInfo(String userId) {
-        return null;
+    public Response getMyInfo(String email) {
+        Response response = new Response();
+
+        try{
+            User user = userRepository.findByEmail(email).orElseThrow(()-> new OurException("User Not Found"));
+            UserDto userDto = Utils.mapUserEntityToUserDTO(user);
+            response.setStatusCode(200);
+            response.setMessage("Successful");
+            response.setUser(userDto);
+
+        }catch (OurException e){
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error getting user information " + e.getMessage());
+        }
+        return response;
     }
 }
--- src/main/java/com/example/Hotel_Management_System/service/interfac/IUserService.java
+++ src/main/java/com/example/Hotel_Management_System/service/interfac/IUserService.java
@@ -17,6 +17,6 @@ public interface IUserService {
 
     Response getUserById(String userId);
 
-    Response getMyInfo(String userId);
+    Response getMyInfo(String email);
 
 }
