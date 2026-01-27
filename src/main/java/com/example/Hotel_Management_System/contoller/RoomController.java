76008a1115e53218f6d75e1f0b14945bddeec357
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-27 21:40:09 +0530

    Create the get room by id   function in the Room controller

--- src/main/java/com/example/Hotel_Management_System/contoller/RoomController.java
+++ src/main/java/com/example/Hotel_Management_System/contoller/RoomController.java
@@ -52,7 +52,11 @@ public class RoomController {
         return roomService.getAllRoomTypes();
     }
 
-
+    @GetMapping("/room-by-id/{roomId}")
+    public ResponseEntity<Response> getRoomById(@PathVariable Long roomId){
+        Response response = roomService.getRoomById(roomId);
+        return  ResponseEntity.status(response.getStatusCode()).body(response);
+    }
 
 
 }
