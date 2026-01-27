0dae8eaebabbd331d7aee28ed5dae9cddb89343c
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-27 22:47:13 +0530

    Create the get all booking function in the Booking controller

--- src/main/java/com/example/Hotel_Management_System/contoller/BookingController.java
+++ src/main/java/com/example/Hotel_Management_System/contoller/BookingController.java
@@ -25,4 +25,12 @@ public class BookingController {
         Response response = bookingService.saveBooking(rooId, userId, bookingRequest);
         return  ResponseEntity.status(response.getStatusCode()).body(response);
     }
+
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllBookings(){
+        Response response = bookingService.getAllBookings();
+        return  ResponseEntity.status(response.getStatusCode()).body(response);
+    }
 }
