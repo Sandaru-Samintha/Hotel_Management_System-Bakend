54e168ecc3416fbdf76302dca3318fa3b323abb9
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-28 12:17:20 +0530

    finalized and finish the back end

--- src/main/java/com/example/Hotel_Management_System/contoller/BookingController.java
+++ src/main/java/com/example/Hotel_Management_System/contoller/BookingController.java
@@ -16,13 +16,13 @@ public class BookingController {
     @Autowired
     private IBookingService bookingService;
 
-    @PutMapping("/book-room/{roomId}/{userId}")
+    @PostMapping("/book-room/{roomId}/{userId}")
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
-    public ResponseEntity<Response> saveBookings(@PathVariable Long rooId,
+    public ResponseEntity<Response> saveBookings(@PathVariable Long roomId,
                                                  @PathVariable Long userId,
                                                  @RequestBody Booking bookingRequest){
 
-        Response response = bookingService.saveBooking(rooId, userId, bookingRequest);
+        Response response = bookingService.saveBooking(roomId, userId, bookingRequest);
         return  ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
--- src/main/java/com/example/Hotel_Management_System/service/impl/BookingService.java
+++ src/main/java/com/example/Hotel_Management_System/service/impl/BookingService.java
@@ -79,7 +79,7 @@ public class BookingService implements IBookingService {
 
         try {
             Booking booking = bookingRepository.findByBookingConfirmationCode(confirmationCode).orElseThrow(()-> new OurException("Booking Not Found"));
-            BookingDto bookingDto = Utils.mapBookingEntityToBookingDTO(booking);
+            BookingDto bookingDto = Utils.mapBookingEntityToBookingDTOPlusBookedRooms(booking,true);
             response.setStatusCode(200);
             response.setMessage("Successful");
             response.setBooking(bookingDto);
